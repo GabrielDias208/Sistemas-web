7f24f20f05d1125ca73f272922f8ea641d2496e6
GabrielDias <164105058+GabrielDias208@users.noreply.github.com>
2026-07-31 15:39:03 -0300

    Fix mostrarTela function to use 'nome' parameter

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function mostrarTela(nome){
-    const telas = document.querySelectorAll('section')
-    telas.forEach(telas => {
-        telas.style.display = 'none';
-    });
+        function mostrarTela(nome){
+           
+            const telas = document.querySelectorAll('section')
+            telas.forEach(telas => {
+                telas.style.display = 'none';
+            });
 
-    const telaEscolhida = document.getElementById('tela-' + none)
-    telaEscolhida.style.display = "block";
-};
+            const telaEscolhida = document.getElementById('tela-' + nome)
+            telaEscolhida.style.display = 'block';
+            
+        }
